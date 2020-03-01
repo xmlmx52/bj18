eb9b996027a9b8f6fd5e11c83a5c0188feebf70e
xmlmx52 <2501661265@qq.com>
2020-03-01 16:53:07 +0800

    login视图

--- view.py
+++ view.py
@@ -1,4 +1,8 @@
 from django.http import Httpresponse
+from django.shortscut import redirect
 
 def index(request):
     return Httpresponse('index')
+
+def login(request):
+    return redirect('/index')
